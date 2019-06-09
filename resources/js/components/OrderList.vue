b419014d4d4d10a96edf5650d7a86de6d4193b29
Lehren <loek-ehren@hotmail.com>
2019-06-09 17:11:14 +0200

    Add orderList component to review your orders

--- resources/js/components/OrderList.vue
+++ resources/js/components/OrderList.vue
@@ -1,4 +1,12 @@
 <template>
+    <div>
+        <h1 class="text-center">Here are your orders</h1>
+        <div class="jumbotron">
+            <p class="lead" v-for="orderItem in orderList">
+                {{getOrderItemText(orderItem)}}
+            </p>
+        </div>
+    </div>
 
 
 
@@ -6,7 +14,21 @@
 
 <script>
     export default {
-        name: "OrderList"
+        name: "OrderList",
+        props: {orderList: {type: Array, default: () => [], required: false}},
+        methods: {
+            getOrderItemText(orderItem) {
+                let text = 'At ' + orderItem.time.format('H:mm:s') + ' you ordered a ' + orderItem.selectedBeverage.name.toLowerCase();
+                if (orderItem.selectedOptions.length > 0) {
+                    text += ' with ';
+                    text += orderItem.selectedOptions.map((selectedOption, index) => {
+                        return selectedOption.name.toLowerCase() + (index === orderItem.selectedOptions.length - 1 ? '' : ' and ');
+                    }).join('');
+                }
+                text += '.';
+                return text;
+            }
+        }
     }
 </script>
 
